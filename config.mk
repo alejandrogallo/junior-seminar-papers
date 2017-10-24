908306e5d79c2522f543f25eeac66fedc51383b5
Alejandro Gallo <aamsgallo@gmail.com>
2017-10-24 13:33:12 +0200

    Add deps

--- config.mk
+++ config.mk
@@ -2,3 +2,5 @@ BUILD_DIR = build
 
 README.md: main.tex ## Create the README.md out of the main.tex
 	$(PANDOC) -f latex -t markdown_github $< -o $@
+
+DEPENDENCIES = README.md $(DEFAULT_DEPENDENCIES)
