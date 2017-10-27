6452cdd16e627043c9a69fe7339c22df577f1ec8
Alejandro Gallo <aamsgallo@gmail.com>
2017-10-27 17:33:39 +0200

    Implement random

--- config.mk
+++ config.mk
@@ -3,4 +3,10 @@ BUILD_DIR = build
 README.md: main.tex ## Create the README.md out of the main.tex
 	-$(PANDOC) -f latex -t markdown_github $< -o $@
 
+choose-random-paper: ## Choose a random paper
+	@grep item papers.tex | \
+	wc -l | \
+	xargs -n1 seq 1 | \
+	sort -R | head -1
+
 DEPENDENCIES = README.md $(DEFAULT_DEPENDENCIES)
